83317a70fc15cb2323f1331b7e59292ac0db236e
Takashi Masuda <masutaka.net@gmail.com>
2015-07-13 00:08:57 +0900

    Add newrelic settings

--- lib/clockwork.rb
+++ lib/clockwork.rb
@@ -1,7 +1,7 @@
 require 'active_support/time'
 require 'erb'
+require 'newrelic_rpm'
 require 'yaml'
-
 require_relative 'blog_subscribers_number'
 require_relative 'blog_subscribers_number/growth_forecast'
 require_relative 'blog_subscribers_number/social_service'
