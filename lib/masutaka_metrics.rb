eacf4656333ae7532eb08810909aa544ed5de24e
Takashi Masuda <masutaka.net@gmail.com>
2016-06-12 18:33:47 +0900

    Also post to fluentd with GrowthForecast

--- lib/masutaka_metrics.rb
+++ lib/masutaka_metrics.rb
@@ -1,4 +1,5 @@
 require 'clockwork'
+require 'fluent-logger'
 require 'logger'
 
 class MasutakaMetrics
@@ -8,6 +9,7 @@ class MasutakaMetrics
     @livedoor_reader = LivedoorReader.new(settings)
     @hatena_bookmark = HatenaBookmark.new(settings)
     @fetch_interval_seconds = settings['fetch_interval_seconds']
+    @fluent_logger = ::Fluent::Logger::FluentLogger.new
     @logger = Logger.new(STDOUT)
   end
 
@@ -19,15 +21,24 @@ class MasutakaMetrics
   private
 
   def post
-    post_feedly
-    post_livedoor_reader
-    post_hatena_bookmark
+    metrics = {
+      'Feedly'           => post_feedly,
+      'HatenaBookmark'   => post_hatena_bookmark,
+      'LiveDwangoReader' => post_livedoor_reader,
+    }
+
+    @logger.info("metrics: #{metrics}")
+
+    unless @fluent_logger.post('masutaka.metrics', metrics)
+      @logger.error("FluentLogger: #{@fluent_logger.last_error}")
+    end
   end
 
   def post_feedly
     subscribers = @feedly.subscribers
     @growth_forecast.post_feedly(subscribers)
     @logger.info("feedly subscribers: #{subscribers}")
+    subscribers
   rescue => e
     @logger.error("feedly subscribers: unknown, class=#{e.class}, backtrace=#{e.backtrace.join(' | ')}")
   end
@@ -36,6 +47,7 @@ class MasutakaMetrics
     subscribers = @livedoor_reader.subscribers
     @growth_forecast.post_livedoor_reader(subscribers)
     @logger.info("livedoor_reader subscribers: #{subscribers}")
+    subscribers
   rescue => e
     @logger.error("livedoor_reader subscribers: unknown, class=#{e.class}, backtrace=#{e.backtrace.join(' | ')}")
   end
@@ -44,6 +56,7 @@ class MasutakaMetrics
     subscribers = @hatena_bookmark.count
     @growth_forecast.post_hatena_bookmark(subscribers)
     @logger.info("hatena_bookmark subscribers: #{subscribers}")
+    subscribers
   rescue => e
     @logger.error("hatena_bookmark subscribers: unknown, class=#{e.class}, backtrace=#{e.backtrace.join(' | ')}")
   end
